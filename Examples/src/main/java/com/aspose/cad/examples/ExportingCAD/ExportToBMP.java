63ee4cf9e2a6c19c461b71d454ff2e004c748504
Muhammad-Adnan-Ahmad <adnan.ahmad@aspose.com>
2017-04-26 17:35:31 +0500

    Added new feature as per Aspose.CAD for Java 17.4 release
    ExportToPDF.java
    ExportToBMP.java
    public class SupportMLeaderEntityForDWGFormat.java

---
+++ Examples/src/main/java/com/aspose/cad/examples/ExportingCAD/ExportToBMP.java
@@ -0,0 +1,38 @@
+package com.aspose.cad.examples.ExportingCAD;
+
+import com.aspose.cad.Image;
+import com.aspose.cad.InterpolationMode;
+import com.aspose.cad.SmoothingMode;
+import com.aspose.cad.TextRenderingHint;
+import com.aspose.cad.examples.Utils.Utils;
+import com.aspose.cad.fileformats.cad.ScaleType;
+import com.aspose.cad.imageoptions.CadRasterizationOptions;
+import com.aspose.cad.imageoptions.PdfOptions;
+import com.aspose.cad.imageoptions.TypeOfEntities;
+
+public class ExportToBMP {
+
+	public static void main(String[] args) {
+		
+		ExStart:ExportToBMP
+                 // The path to the resource directory.
+		String dataDir = Utils.getDataDir(ExportToBMP.class) + "ExportingCAD/";
+                String fileName = getFileFromDesktop("APFH Floor Plan.dwf");
+               
+            com.aspose.cad.Image image = com.aspose.cad.Image.load(fileName);
+        {
+            BmpOptions bmpOptions = new BmpOptions();
+            DwfRasterizationOptions cadRasterizationOptions = new DwfRasterizationOptions();
+            bmpOptions.setVectorRasterizationOptions(cadRasterizationOptions);
+            cadRasterizationOptions.setCenterDrawing(true);
+            cadRasterizationOptions.setPageHeight(500);
+            cadRasterizationOptions.setPageWidth(500);
+            cadRasterizationOptions.setLayouts(new String[] { "Model" });
+            // export
+            String outPath = fileName + ".bmp";
+            image.save(outPath, bmpOptions);
+          }		
+	 ExEnd:ExportToBMP
+
+}
+}
---
+++ Examples/src/main/java/com/aspose/cad/examples/ExportingCAD/ExportToPDF.java
@@ -0,0 +1,39 @@
+package com.aspose.cad.examples.ExportingCAD;
+
+import com.aspose.cad.Image;
+import com.aspose.cad.InterpolationMode;
+import com.aspose.cad.SmoothingMode;
+import com.aspose.cad.TextRenderingHint;
+import com.aspose.cad.examples.Utils.Utils;
+import com.aspose.cad.fileformats.cad.ScaleType;
+import com.aspose.cad.imageoptions.CadRasterizationOptions;
+import com.aspose.cad.imageoptions.PdfOptions;
+import com.aspose.cad.imageoptions.TypeOfEntities;
+
+public class ExportToPDF {
+
+	public static void main(String[] args) {
+		
+		ExStart:ExportToPDF
+                 // The path to the resource directory.
+		String dataDir = Utils.getDataDir(ExportToPDF.class) + "ExportingCAD/";
+		
+		
+              String fileName = getFileFromDesktop("APFH Floor Plan.dwf");
+               com.aspose.cad.Image image = com.aspose.cad.Image.load(fileName);
+        {
+            BmpOptions bmpOptions = new BmpOptions();
+            DwfRasterizationOptions cadRasterizationOptions = new DwfRasterizationOptions();
+            bmpOptions.setVectorRasterizationOptions(cadRasterizationOptions);
+            cadRasterizationOptions.setCenterDrawing(true);
+            cadRasterizationOptions.setPageHeight(500);
+            cadRasterizationOptions.setPageWidth(500);
+            cadRasterizationOptions.setLayouts(new String[] { "Model" });
+            // export
+            String outPath = fileName + ".bmp";
+            image.save(outPath, bmpOptions);
+          }		
+	 ExEnd:ExportToPDF
+
+}
+}

5f59baca87037ea3532204437bb361c70eca1e96
Ahmad <m.ahmadchishti@gmail.com>
2020-10-16 18:27:43 +0500

    Updated to version 20.9

--- Examples/src/main/java/com/aspose/cad/examples/DWGDrawings/ImportImageToDWG.java
+++ Examples/src/main/java/com/aspose/cad/examples/DWGDrawings/ImportImageToDWG.java
@@ -10,10 +10,7 @@ import com.aspose.cad.examples.DXFDrawings.RenderDXFAsPDF;
 import com.aspose.cad.examples.Utils.Utils;
 import com.aspose.cad.fileformats.cad.CadDrawTypeMode;
 import com.aspose.cad.fileformats.cad.CadImage;
-import com.aspose.cad.fileformats.cad.cadobjects.Cad2DPoint;
-import com.aspose.cad.fileformats.cad.cadobjects.CadBaseObject;
-import com.aspose.cad.fileformats.cad.cadobjects.CadRasterImage;
-import com.aspose.cad.fileformats.cad.cadobjects.CadRasterImageDef;
+import com.aspose.cad.fileformats.cad.cadobjects.*;
 import com.aspose.cad.imageoptions.CadRasterizationOptions;
 import com.aspose.cad.imageoptions.PdfOptions;
 
@@ -25,24 +22,20 @@ public static void main(String[] args) {
 
         String dataDir = Utils.getDataDir(ImportImageToDWG.class) + "DWGDrawings/";
         //ExStart:ImportImageToDWG
-        String srcFile = dataDir + "conic_pyramid.dxf";
+        String srcFile = dataDir + "Drawing11.dwg";
 
         Image image = Image.load(srcFile);
 
-        CadRasterImageDef cadRasterImageDef = new CadRasterImageDef();
+        CadRasterImageDef cadRasterImageDef = new CadRasterImageDef("road-sign-custom.png", 640, 562);
         cadRasterImageDef.setObjectHandle("A3B4");
-        cadRasterImageDef.setFileName("road-sign-custom.png");
-        CadRasterImage cadRasterImage = new CadRasterImage();
+
+        Cad3DPoint insertionPoint = new Cad3DPoint(26.77, 22.35);
+        Cad3DPoint uVector = new Cad3DPoint(0.0061565450840500831, 0);
+        Cad3DPoint vVector = new Cad3DPoint(0, 0.0061565450840500822);
+
+        CadRasterImage cadRasterImage = new CadRasterImage(cadRasterImageDef, insertionPoint, uVector, vVector);
         cadRasterImage.setImageDefReference("A3B4");
-        cadRasterImage.getInsertionPoint().setX(26.77);
-        cadRasterImage.getInsertionPoint().setY(22.35);
         cadRasterImage.setDisplayFlags((short)7);
-        cadRasterImage.setImageSizeU(640);
-        cadRasterImage.setImageSizeV(562);
-        cadRasterImage.getUVector().setX(0.0061565450840500831);
-        cadRasterImage.getUVector().setY(0);
-        cadRasterImage.getVVector().setX(0);
-        cadRasterImage.getVVector().setY(0.0061565450840500822);
         cadRasterImage.setClippingState((short)0);
         cadRasterImage.getClipBoundaryVertexList().add(new Cad2DPoint(-0.5, 0.5));
         cadRasterImage.getClipBoundaryVertexList().add(new Cad2DPoint(639.5, 561.5));
